df05829e70e6bd116d881b07b9e7465eba6f6e01
blasid <damianblasi@gmail.com>
2015-03-26 17:13:16 -0600

    Update didinga.py

--- Languages/didinga.py
+++ Languages/didinga.py
@@ -1,3 +1,4 @@
+"""
 @article{rosatodidinga1980,
     author = {Michele Rosato},
     title = {Didinga grammar and dictionary},
@@ -6,8 +7,7 @@
     year = {1980},
     localfile = {afr5}
 }
-
-
+"""
 
 ## 1 is also kodesong but cannot be used in compund numbers above 10 ##
 f[1] = "kodei"
@@ -15,11 +15,11 @@ f[2] = "rama"
 f[3] = "iyo"
 f[4] = "wec"
 f[5] = "tur"
+f[1*5+r] = f[5]+"ki"+f[r]
 f[6] = "turkonon"
 f[10] = "omoto"
 ##7,8,9 have ki to create 5 plus r where r is 2, 3, and 4##
-f[r+5] = f[5]+"ki"+f[r]
-f[10+r] = f[10] +ki+ f[r]
+f[1*10+r] = f[10] +ki+ f[r]
 f[20] = "ken", "rama"
-f[20+r] = f[20] + "ki" + f[r]
+f[1*20+r] = f[20] + "ki" + f[r]
 
