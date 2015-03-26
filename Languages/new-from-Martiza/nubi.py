f2bc76a6c5b8243e697b6d1284207281cf71925d
blasid <damianblasi@gmail.com>
2015-03-26 17:17:08 -0600

    Update nubi.py

--- Languages/new-from-Martiza/nubi.py
+++ Languages/new-from-Martiza/nubi.py
@@ -16,17 +16,17 @@ f[5] = "kamsa"
 f[6] = "sita"
 f[7] = "saba"
 f[8] = "tamania"
-f[9] = "tisa
+f[9] = "tisa"
 f[10] = "ashara"
 f[11] = "ida", f[10]
 f[12] = "idna", f[10]
-f[10+r] = f[r],f[10] 
+f[1*10+r] = f[r],f[10] 
 f[20] = "ishirin"
 f[30] = "teletin"
-f[k*10] = f[r], "in"
+f[p*10+0] = f[p], "in"
 #for numbers 40, 70, and 90 the a in the original form of the number is changed to an e before adding the ending"
-f[20+r] = f[r], f[20]
-f[30+r] = f[r], f[20]
-f[k*10+r] = f[r], f[k*10],
+f[1*20+r] = f[r], f[20]
+f[1*30+r] = f[r], f[20]
+f[p*10+r] = f[r], f[p*10],
 #1 becomes waid when added to the tens#
 f[100] = "mia"
