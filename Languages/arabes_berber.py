4daae0a46c2c8a6966ff41afc2bca3e54024937f
blasid <damianblasi@gmail.com>
2015-03-26 17:03:27 -0600

    Update arabes_berber.py

--- Languages/arabes_berber.py
+++ Languages/arabes_berber.py
@@ -1,3 +1,4 @@
+"""
 @article{noticesreinaud1861
     author = {Par M. Reinaud},
     title = {Les Dictionnaires Geographiques Arabes et Sur le systeme Primitif de la Numeration Chez chez le peuples de race berbere},
@@ -6,15 +7,16 @@
     year = {1861},
     localfile = {afr10}
 }
-
+'''
+"""
 
 f[1] = "ighem"
 f[2] = "tzem"
 f[3] = "charet"
 f[4] = "occas"
 f[5] = "fous"
+f[1*5+r] = f[5] + f[r]
 f[10] = "meraoun"
-f[5+r] = f[5] + f[r]
-f[10+r] = f[10] + f[r]
-f[k*10] = f[k]+f[10]
-f[k*10+r] = f[k] + f[10] + f[r]
+f[1*10+r] = f[10] + f[r]
+f[p*10+0] = f[p] + f[10]
+f[p*10+r] = f[p] + f[10] + f[r]
